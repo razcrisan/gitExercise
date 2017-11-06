8458469e09d24cbb67f969f4068d2cbedd779032
razcrisan <rcrisan13@gmail.com>
2017-11-06 13:52:09 -0500

    second commit

--- gitCloningExercise/Program.cs
+++ gitCloningExercise/Program.cs
@@ -10,6 +10,10 @@ namespace gitCloningExercise
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 1; i++)
+            {
+                Console.WriteLine("Hello World.");
+            }
         }
     }
 }
